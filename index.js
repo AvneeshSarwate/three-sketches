06399e9be4fc4ceb3d1c4b3129a957c5812bbad4
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-11-25 00:44:28 -0500

    cleaning up

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import * as Sketch from "./sketches/yegor_painting_morph.js";
+import * as Sketch from "./sketches/moving_plane_shaded.js";
 import * as twgl from "./twgl-full.js";
-
-// Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>JS Starterrrrr</h1>`;
 
 console.log("twgl import test", twgl.createFramebufferInfo);
 
