7e44d76af7f2034fe72759f1a999e9f73514c952
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-11-29 20:44:57 -0500

    adding sketches for playing with UV generation/mapping thru vertex shader

---
+++ sketches/uv_experiments/instancing_debug.js
@@ -0,0 +1,223 @@
+/*
+an attempt at replicating this example - https://github.com/mrdoob/three.js/blob/dev/examples/webgl_buffergeometry_instancing.html
+using a CircleBufferGeometry as the source geometry to instance
+
+*/
+
+
+import * as THREE from '../../three.module.js';
+import Stats from '../../stats.module.js';
+
+//template literal function for use with https://marketplace.visualstudio.com/items?itemName=boyswan.glsl-literal
+//backup fork at https://github.com/AvneeshSarwate/vscode-glsl-literal
+const glsl = a => a[0];
+
+let container, stats;
+
+let camera, scene, renderer;
+
+function init() {
+
+    container = document.getElementById('container');
+
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10);
+    camera.position.z = 2;
+
+    scene = new THREE.Scene();
+
+    // geometry
+
+    const vector = new THREE.Vector4();
+
+    const instances = 10;
+
+    const positions = [];
+    const offsets = [];
+    const colors = [];
+    const orientationsStart = [];
+    const orientationsEnd = [];
+
+    positions.push(0.025, - 0.025, 0);
+    positions.push(- 0.025, 0.025, 0);
+    positions.push(0, 0, 0.025);
+
+    // instanced attributes
+
+    for (let i = 0; i < instances; i++) {
+
+        // offsets
+
+        offsets.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+
+        // colors
+
+        colors.push(Math.random(), Math.random(), Math.random(), Math.random());
+
+        // orientation start
+
+        vector.set(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
+        vector.normalize();
+
+        orientationsStart.push(vector.x, vector.y, vector.z, vector.w);
+
+        // orientation end
+
+        vector.set(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
+        vector.normalize();
+
+        orientationsEnd.push(vector.x, vector.y, vector.z, vector.w);
+
+    }
+
+    const geometry = new THREE.InstancedBufferGeometry();
+    geometry.instanceCount = instances; 
+
+    let useCircleGeometry = true;
+    if(useCircleGeometry){
+        let geo2 = new THREE.CircleGeometry(0.25, 32); //instead of CircleBufferGeometry
+        // let geo2 = new THREE.PlaneGeometry(0.25, 0.25, 10, 10); //instead of PlaneBufferGeometry
+        // geometry.setAttribute('position', new THREE.Float32BufferAttribute(geo2.attributes.position.array, 3));
+        geometry.fromGeometry(geo2);
+    } else {
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));   
+    }
+
+    geometry.setAttribute('offset', new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3));
+    geometry.setAttribute('color', new THREE.InstancedBufferAttribute(new Float32Array(colors), 4));
+    geometry.setAttribute('orientationStart', new THREE.InstancedBufferAttribute(new Float32Array(orientationsStart), 4));
+    geometry.setAttribute('orientationEnd', new THREE.InstancedBufferAttribute(new Float32Array(orientationsEnd), 4));
+
+    // material
+
+    const material = new THREE.RawShaderMaterial({
+
+        uniforms: {
+            "time": { value: 1.0 },
+            "sineTime": { value: 1.0 }
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        side: THREE.DoubleSide,
+        transparent: true
+
+    });
+
+    //
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    if (renderer.capabilities.isWebGL2 === false && renderer.extensions.has('ANGLE_instanced_arrays') === false) {
+
+        document.getElementById('notSupported').style.display = '';
+        return;
+
+    }
+
+    //
+
+
+    //
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    render();
+    stats.update();
+
+}
+
+let startTime = performance.now();
+
+function render() {
+
+    const time = performance.now() - startTime;
+
+    const object = scene.children[0];
+
+    object.rotation.y = time * 0.0005;
+    object.material.uniforms["time"].value = time * 0.005;
+    object.material.uniforms["sineTime"].value = Math.sin(object.material.uniforms["time"].value * 0.05);
+
+    renderer.render(scene, camera);
+
+}
+
+export { init, animate };
+
+const vertexShader = glsl`
+precision highp float;
+
+uniform float sineTime;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+attribute vec3 position;
+attribute vec3 offset;
+attribute vec4 color;
+attribute vec4 orientationStart;
+attribute vec4 orientationEnd;
+
+varying vec3 vPosition;
+varying vec4 vColor;
+
+void main(){
+
+    vPosition = offset * max( abs( sineTime * 2.0 + 1.0 ), 0.5 ) + position;
+    vec4 orientation = normalize( mix( orientationStart, orientationEnd, sineTime ) );
+    vec3 vcV = cross( orientation.xyz, vPosition );
+    vPosition = vcV * ( 2.0 * orientation.w ) + ( cross( orientation.xyz, vcV ) * 2.0 + vPosition );
+
+    vColor = color;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
+
+}
+`;
+
+const fragmentShader = glsl`
+precision highp float;
+
+uniform float time;
+
+varying vec3 vPosition;
+varying vec4 vColor;
+
+void main() {
+
+    vec4 color = vec4( vColor );
+    color.r += sin( vPosition.x * 10.0 + time ) * 0.5;
+
+    gl_FragColor = color;
+
+}
+`;
---
+++ sketches/uv_experiments/sketch_instanced.js
@@ -0,0 +1,171 @@
+import * as THREE from "../../three.module.js";
+import Stats from "../../stats.module.js";
+
+//template literal function for use with https://marketplace.visualstudio.com/items?itemName=boyswan.glsl-literal
+//backup fork at https://github.com/AvneeshSarwate/vscode-glsl-literal
+const glsl = a => a[0];
+
+let camera, scene, renderer, stats;
+
+let startTime = performance.now()/1000;
+let uniforms, time;
+
+let gridSize = 10; Math.floor(4 + (1- Math.random()**2)*36);
+
+
+function init() {
+    const container = document.getElementById("container");
+
+    camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    scene = new THREE.Scene();
+
+    const tileSize = 1 / gridSize * 0.98;
+
+    let geometry = new THREE.CircleGeometry(tileSize, 32);
+    // let geometry = new THREE.PlaneBufferGeometry(2/gridSize, 2/gridSize);
+    // let geometry = new THREE.SphereBufferGeometry(tileSize, 10, 10);
+
+    window.geometry = geometry;
+
+    const geometryInstanced = new THREE.InstancedBufferGeometry();
+
+    const painting_texture = new THREE.TextureLoader().load("./yegor_painting.jpg");
+
+    uniforms = {
+        time: { value: 1.0 },
+        gridSize: { value: gridSize },
+        painting: { value: painting_texture }
+    };
+
+    const material = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    });
+
+    // let positions = geometry.attributes.position.array;
+    let xIndices = [];
+    let yIndices = [];
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            xIndices.push(i);
+            yIndices.push(j);
+        }
+    }
+
+    geometryInstanced.fromGeometry(geometry);
+    // geometryInstanced.setAttribute('position', geometry.attributes.position);
+    // geometryInstanced.setAttribute('normal', geometry.attributes.normal);
+    // geometryInstanced.setAttribute('uv', geometry.attributes.uv);
+    geometryInstanced.setAttribute('xInd', new THREE.InstancedBufferAttribute(new Float32Array(xIndices), 1))
+    geometryInstanced.setAttribute('yInd', new THREE.InstancedBufferAttribute(new Float32Array(yIndices), 1))
+
+    const mesh = new THREE.Mesh(geometryInstanced, material);
+    scene.add(mesh);
+    
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio / 2);
+    container.appendChild(renderer.domElement);
+
+    onWindowResize();
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+//
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    time = performance.now() / 1000 - startTime;
+    uniforms.time.value = time;
+
+    renderer.render(scene, camera);
+    stats.update();
+}
+
+export { init, animate };
+
+let vertexShader = glsl`
+varying vec2 vUv;
+varying float xInd_v;
+varying float yInd_v;
+
+attribute float xInd;
+attribute float yInd;
+
+uniform float gridSize;
+uniform float time;
+
+//convert grid index of circle geometry to normalized circle position
+float ind2pos(float i){
+    return mix(-1., 1., i/gridSize) + 1./gridSize;
+}
+
+float sinN(float n){ return (sin(n)+1.)/2.;}
+
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+  }
+
+void main()	{
+
+  vUv = uv;
+
+  float indN = (xInd*gridSize + yInd)/(gridSize*gridSize);
+  float randN = rand(vec2(xInd, yInd));
+
+  vec3 dev = vec3(ind2pos(xInd), ind2pos(yInd), 0.);
+  vec3 dev_debug = vec3(mix(-.1, .1, xInd/gridSize), mix(-.1, .1, yInd/gridSize), 0.);
+  vec3 dev_debug2 =  vec3(sin(time+indN*3.1415), cos(time+indN*3.1415), 0)*0.1;
+
+  vec3 motion = vec3(sin(time*(1.+randN*1.)), cos(time*(1.+randN*1.)), 0)*3./gridSize*0.;
+
+  vec3 p = position + dev + mix(motion, vec3(0), pow(sinN(time+yInd/gridSize*3.1415), 6.));
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(p, 1.0 );
+  
+  xInd_v = xInd;
+  yInd_v = yInd;
+}`;
+
+let fragmentShader = glsl`
+varying vec2 vUv;
+varying float xInd_v;
+varying float yInd_v;
+
+uniform float time;
+uniform sampler2D painting;
+uniform float gridSize;
+
+float sinN(float n){
+    return (sin(n)+1.)/2.;
+}
+
+void main()	{
+
+  vec2 uv = mix(vUv, (vUv-.5) * 1., sinN(time)*0.);
+  vec2 cellCoord = vec2(xInd_v/gridSize + uv.x/gridSize, yInd_v/gridSize + uv.y/gridSize);
+  vec4 paintCell = texture(painting, cellCoord);
+  vec4 debugColor = vec4(xInd_v/gridSize, yInd_v/gridSize, 0.5, 1);
+  vec4 debugColor2 = vec4(cellCoord.x, cellCoord.y, 0.5, 1);
+
+  gl_FragColor = paintCell;
+
+}
+`;
+
+/*
+Tile the painting onto a bunch of rects, and then have those rects swap places with each other
+- alternate row and column "rotation" of a randomly selected row/column
+- tile and end flies backwards over its row/column, while simultaneously the rest move one step fwd
+
+*/
