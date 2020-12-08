9aa7a45db80876549398de62b645c402bbac9296
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-12-08 01:25:17 -0500

    getting accurate scaling of sampling area of circle with uv's

--- sketches/render_target_experiments/sketch2.js
+++ sketches/render_target_experiments/sketch2.js
@@ -14,33 +14,34 @@ let alphaTexture = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHe
 //backup fork at https://github.com/AvneeshSarwate/vscode-glsl-literal
 const glsl = a => a[0];
 
-let gridSize = 40;
+let gridSize = 30;
 
 let lerp = (v1, v2, a) => (1 - a) * v1 + a * v2;
 let sinN = n => (Math.sin(n)+1/2);
 
 function createTile(i, j, gridSize, referenceMaterial, painting_texture) {
-    let tileSize = 2 / gridSize * 0.9;
-    const geometry = new THREE.CircleBufferGeometry(tileSize*2, 32);
+    let tileSize = 1 / gridSize * 2;
+    const geometry = new THREE.CircleBufferGeometry(tileSize, 32);
     const newMaterial = referenceMaterial.clone();
 
     newMaterial.uniforms.xInd = { value: i };
     newMaterial.uniforms.yInd = { value: j };
     newMaterial.uniforms.gridSize = { value: gridSize };
     newMaterial.uniforms.painting = { value: painting_texture };
+    newMaterial.uniforms.tileSize = { value: tileSize}
 
     let tileMesh = new THREE.Mesh(geometry, newMaterial);
-    let xRoot = tileMesh.position.x = lerp(-1, 1, i / gridSize) + tileSize / 2;
-    let yRoot = tileMesh.position.y = lerp(-1, 1, j / gridSize) + tileSize / 2;
+    let xRoot = tileMesh.position.x = lerp(-1, 1, i / gridSize) + 1/gridSize;
+    let yRoot = tileMesh.position.y = lerp(-1, 1, j / gridSize) + 1/gridSize;
     tileMesh.position.z = 0;
 
     let cellPhase = Math.random()*Math.PI*2;
-    let dev = Math.random() * 5;
+    let dev = (1 + Math.random()) * 1;
 
     tileMesh.onBeforeRender = function(renderer, scene, camera, geometry, material, group){
         tileMesh.position.x = xRoot + Math.cos(time + cellPhase)*(1/gridSize/2) * dev;
         tileMesh.position.y = yRoot + Math.sin(time + cellPhase)*(1/gridSize/2) * dev;
-        // tileMesh.scale.x = tileMesh.scale.y = 0.6 + sinN(time * dev)*0.5;
+        tileMesh.scale.x = tileMesh.scale.y = 1; 0.6 + sinN(time * dev)*0.5;
     }
 
     return tileMesh;
@@ -75,7 +76,7 @@ function createPaintingSamplerScene() {
 
 
     meshes.forEach(mesh => {
-        mesh.renderOrder = Math.random();
+        // mesh.renderOrder = Math.random();
         paintingScene.add(mesh);
     });
 }
@@ -89,8 +90,8 @@ function createPlaneSamplingScene(){
     }
     let samplingMaterial = new THREE.ShaderMaterial({
         uniforms: uniforms2,
-        vertexShader:header_code + vertexShader,
-        fragmentShader: textureWarpShader
+        vertexShader: vertexShader,
+        fragmentShader: header_code + textureWarpShader
     });
     let planeMesh = new THREE.Mesh(planeGeometry, samplingMaterial);
     warpScene.add(planeMesh);
@@ -163,15 +164,13 @@ uniform sampler2D painting;
 uniform float xInd;
 uniform float yInd;
 uniform float gridSize;
-
-float sinN(float n){
-    return (sin(n)+1.)/2.;
-}
+uniform float tileSize;
 
 void main()	{
 
     vec2 uv = vUv;
-    vec2 cellCoord = vec2(xInd/gridSize + uv.x/gridSize, yInd/gridSize + uv.y/gridSize);
+    float gridShift = -1./gridSize/2.;
+    vec2 cellCoord = vec2(xInd/gridSize + gridShift + uv.x*tileSize, yInd/gridSize + gridShift + uv.y*tileSize);
     vec4 paintCell = texture(painting, cellCoord);
 
     gl_FragColor = paintCell;
@@ -185,13 +184,12 @@ varying vec2 vUv;
 uniform float time;
 uniform sampler2D scene;
 
-float sinN(float n){
-    return (sin(n)+1.)/2.;
-}
-
 void main()	{
     float tm = time * 0.5;
-    vec3 samp = texture(scene, vUv).rgb * vec3(sinN(tm), sinN(-tm*0.39+1.+vUv.x*4.5), sinN(tm*0.95+3.));
+    vec2 sinT = vec2(pow(sinN(time), 5.), pow(sinN(time+PI), 5.));
+    vec2 uv = vec2(quant(vUv.x, 1000.*sinT.x+20.), quant(vUv.y, 1000.*sinT.y+20.));
+    vec3 colorTint = vec3(1); vec3(sinN(tm), sinN(-tm*0.39+1.+vUv.x*4.5), sinN(tm*0.95+3.));
+    vec3 samp = texture(scene, uv).rgb * colorTint;
     gl_FragColor = vec4(samp, 1);
 
 }
