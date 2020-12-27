60820296a8a9297c58e93c0a5a9e03ab218907ab
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-12-27 17:42:54 -0500

    basic feedback effect working

--- sketches/render_target_experiments/index.js
+++ sketches/render_target_experiments/index.js
@@ -6,6 +6,7 @@ if(sketchId == '2') sketchFile = './sketch2.js';
 if(sketchId == '3') sketchFile = './sketch3.js';
 if(sketchId == '4') sketchFile = './sketch4.js';
 if(sketchId == '5') sketchFile = './sketch5.js';
+if(sketchId == '6') sketchFile = './sketch6.js';
 
 import(sketchFile).then(Sketch => {
     Sketch.init();
