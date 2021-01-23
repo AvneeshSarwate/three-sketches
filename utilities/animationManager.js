41b9f8dca6f83d04b41f4d05de23113f180848e8
Avneesh Sarwate <avneeshsarwate@gmail.com>
2021-01-23 18:14:36 -0500

    draft of animation manager

--- utilities/animationManager.js
+++ utilities/animationManager.js
@@ -2,13 +2,45 @@
 
 
 class GestureManager {
+    constructor(){
+        //store all gestures? could get shitty for long running things with programatiaclly generated gestures
+        this.gestures = []; 
 
+        this.numGestures = 0;
+        this.keyMap = {};
+
+        this.playingGestures = new Set();
+    }
+    
+
+    add(key, gesture, storeById=false) {
+        if(storeById) this.gestures.push(gesture);
+        gesture.id = this.numGestures++; 
+        this.keyMap[key] = gesture;
+    }
+
+    //need to call this every frame to run the gestures defined
+    tick() {
+        for(let g in this.playingGestures) {
+            g.step();
+        }
+    }
 }
 
+let gestureManager = new GestureManager();
 
 /*
-    TODO - composable - figure out how to make this composable 
+    TODO - composable - figure out how to make this composable like generator gesture interface from old projects
 */
+
+/*
+    TODO- coordination - implement an interface for "choke groups" like in ableton?
+ */
+
+/*
+   setting up gestures as generators allows set up to be a bit more elegant - the setup happens once in 
+   the beinning of the function, while the "action" happens in the loop of the generator
+ */
 
 class Gesture {
     /**
@@ -16,14 +48,21 @@ class Gesture {
      *      - is a function so you can grab time values coming in over OSC from another application
      * gestureFunc has arguments (currentTime, delta, phase, this_Gesture)
     */
-    constructor(timeFunc, gestureFunc, duration){
-        this.timeFunc = timeFunc;
+    constructor(key, gestureFunc, duration, timeFunc, numLoop, setupFunc){
+        this.key = key;
         this.gestureFunc = gestureFunc;
+        this.duration = duration ? duration : 1;
+        this.timeFunc = timeFunc ? timeFunc : () => Date.now()/1000;
+        this.numLoop = numLoop ? numLoop : Number.POSITIVE_INFINITY;
+        this.setupFunc = setupFunc ? setupFunc : a => a; 
+
         this.lastTime = 0;
         this.time = 0;
         this.startTime = 0;
-        this.duration = duration;
         this.isPlaying = false;
+        this.id = null; //set by the gesture manager
+
+        gestureManager.add(key, this);
 
         /*
             TODO - resumable: have a fields that represents a timeline that only progresses when the animation is
@@ -33,14 +72,29 @@ class Gesture {
         this.accumulatedTime = 0;
     }
 
+    //TODO - resumable - clean up semantics of start/stop/pause/resume wrt various instance variables and resetting
     start() {
+        this.setupFunc();
         this.startTime = this.timeFunc();
         this.time = this.timeFunc(); //this might enable the note in TODO - resumable?
         this.isPlaying = true;
+        this.reset();
+        gestureManager.playingGestures.add(this);
+    }
+    
+    pause() {
+        this.isPlaying = false;
+        gestureManager.playingGestures.delete(this);
     }
 
-    stop() {
-        this.isPlaying = false;
+    resume() {
+        this.isPlaying = true;
+        gestureManager.playingGestures.add(this);
+    }
+
+    reset() {
+        this.accumulatedPhase = 0; 
+        this.accumulatedTime = 0;
     }
 
     step() {
@@ -53,7 +107,16 @@ class Gesture {
             this.accumulatedTime += delta;
             this.accumulatedPhase = (this.accumulatedPhase + delta/this.duration) % 1;
 
-            this.gestureFunc(this.time, delta, phase, this);
+            this.gestureFunc(this.accumulatedTime, this.accumulatedPhase, delta, this);
+        }
+        if(this.accumulatedTime > this.duration * this.numLoop) {
+            this.isPlaying = false;
+            gestureManager.playingGestures.delete(this);
         }
     }
+}
+
+export {
+    Gesture,
+    gestureManager
 }
