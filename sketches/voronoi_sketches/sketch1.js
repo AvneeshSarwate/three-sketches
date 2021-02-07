827d7b2084808e157bbe26135bb6107aa08507fe
Avneesh Sarwate <avneeshsarwate@gmail.com>
2021-02-07 01:03:25 -0500

    basic voronoi geometry working

--- sketches/voronoi_sketches/sketch1.js
+++ sketches/voronoi_sketches/sketch1.js
@@ -2,64 +2,116 @@ import { Voronoi } from "../../libs/rhill-voronoi-core.js"
 import { SimplexNoise } from "../../node_modules/three/examples/jsm/math/SimplexNoise.js"
 import * as THREE from "../../node_modules/three/build/three.module.js";
 import { getCellPoints } from "../../utilities/voronoi_manager.js";
-import { Earcut } from "../../node_modules/three/src/extras/Earcut.js"
+import { Earcut } from "../../node_modules/three/src/extras/Earcut.js";
+import Stats from "../../node_modules/three/examples/jsm/libs/stats.module.js";
 
 
 function range(size, startAt = 0) {
     return [...Array(size).keys()].map(i => i + startAt);
 }
 
+let randColor = () =>  '#'+Math.floor(Math.random()*16777215).toString(16);
 
 let simplex = new SimplexNoise();
-let numSites = 10;
+let numSites = range(10);
+let voronoi = new Voronoi();
 let voronoiSceneComponents = {
     sites: [],
     scene: {},
     geometries: [],
     materials: [],
-    uniforms: {}
+    uniforms: {},
+    meshes:  []
 };
-let time;
 
+let cam, time, renderer, stats, diagram;
+
+function recomputeVoronoi() {
+    return voronoi.compute(voronoiSceneComponents.sites, {xl: -1, xr: 1, yt: -1, yb: 1}) //TODO: might need to flip y
+}
 
 function createVoronoiScene() {
-    voronoiSceneComponents.sites = range(timeNoise2d(0));
-    voronoiSceneComponents.geometries
+    let vsc = voronoiSceneComponents;
+    vsc.sites = numSites.map(i => timeNoise2d(51.32, 21.32, 0-i));
+    vsc.geometries = numSites.map(() => new THREE.BufferGeometry());
+    vsc.materials = numSites.map(() => new THREE.MeshBasicMaterial({color: randColor()}));
+    vsc.meshes = numSites.map(i => new THREE.Mesh(vsc.geometries[i], vsc.materials[i]))
+
+    diagram = recomputeVoronoi();
+
+    vsc.scene = new THREE.Scene();
+
+    numSites.forEach(i => {
+        updateGeometryFromVoronoiCell(diagram.cells[i], vsc.geometries[i]);
+        vsc.scene.add(vsc.meshes[i]);
+    });
 }
 
 function updateVoronoiScene(time) {
-    let {sites, scene, geometries, materials, uniforms} = voronoiSceneComponents;
-    let newSites = sites.map((e, i) => timeNoise2d(51.32, 21.32, time-i));
+    let vsc = voronoiSceneComponents;
+    vsc.sites = numSites.map(i => timeNoise2d(51.32, 21.32, time-i));
 
-    voronoiSceneComponents.sites = newSites;
+    voronoi.recycle(diagram);
+    diagram = recomputeVoronoi();
+
+    numSites.map(i => {
+        updateGeometryFromVoronoiCell(diagram.cells[i], vsc.geometries[i]);
+    });
 }
 
 function updateGeometryFromVoronoiCell(cell, bufferGeom) {
     let cellPts = getCellPoints(cell);
     let triangulatedPts = Earcut.triangulate(cellPts.flat());
-    let uvPts = Float32Array.from(triangulatedPts.map(([x, y]) => [(x+1)/2, (y+1)/2]).flat());
-    let cell3d = Float32Array.from(triangulatedPts.map(([x, y]) => [x, y, 0]).flat());
+    let uvPts = Float32Array.from(cellPts.map(([x, y]) => [(x+1)/2, (y+1)/2]).flat());
+    let cell3d = Float32Array.from(cellPts.map(([x, y]) => [x, y, 0]).flat());
 
+    bufferGeom.setIndex(triangulatedPts);
     bufferGeom.setAttribute('position', new THREE.BufferAttribute(cell3d, 3));
-    bufferGeom.setAttribute('uv', new THREE.BufferAttribute(uvPts, 3));
+    bufferGeom.setAttribute('uv', new THREE.BufferAttribute(uvPts, 2));
 }
 
 function init() {
-    
+
+    createVoronoiScene();
+
+    cam = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+    const container = document.getElementById("container");
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.appendChild(renderer.domElement);
+
+    onWindowResize();
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function timeNoise2d(xRand, yRand, time){
-    return [simplex.noise(xRand, time), simplex.noise(yRand, time)];
+    return {x: simplex.noise(xRand, time), y: simplex.noise(yRand, time)};
 }
 
 function animate() {
-    time = Date.now() / 1000;
+    requestAnimationFrame(animate);
+
+    time = Date.now() / 1000 * 0.2;
    
+    updateVoronoiScene(time);
 
+    renderer.render(voronoiSceneComponents.scene, cam);
 
+    stats.update();
 }
-
-
 
 export {
     init,
